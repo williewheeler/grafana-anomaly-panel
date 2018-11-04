5cda715bb4fcd3db396e09caa33d95a604debb4f
Willie Wheeler <willie.wheeler@gmail.com>
2018-11-04 15:44:46 +1000

    Added license

--- src/anomaly_ctrl.js
+++ src/anomaly_ctrl.js
@@ -5,17 +5,35 @@ import './css/anomaly-panel.css';
 const panelDefaults = {
 };
 
+// http://docs.grafana.org/plugins/developing/development/
 export class AnomalyCtrl extends PanelCtrl {
   constructor($scope, $injector) {
     super($scope, $injector);
     _.defaultsDeep(this.panel, panelDefaults);
   }
 
+  // Grafana event handler: Add tabs when editing a panel
   onInitEditMode() {
     // TODO
   }
 
+  // Grafana event handler: Cleanup
   onPanelTeardown() {
+    // TODO
+  }
+
+  // Grafana event handler: Event triggered on data refresh
+  onDataReceived() {
+    // TODO
+  }
+
+  // Grafana event handler: Event triggered to load data when in snapshot mode
+  onDataSnapshotLoad() {
+    // TODO
+  }
+
+  // Grafana event handler: Handles errors on dashboard refresh
+  onDataError() {
     // TODO
   }
 
